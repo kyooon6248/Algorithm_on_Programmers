e9916f739d340748663c322fd842fd0edc136433
kyooon6248 <62235442+kyooon6248@users.noreply.github.com>
2023-05-10 17:44:28 +0900

    [unrated] Title: 할 일 목록, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.py
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.py
@@ -1,6 +1,7 @@
 def solution(todo_list, finished):
     answer = []
     for i, b in enumerate(finished):
-        if int(b) == 0:
-            answer.append(todo_list[i])
+        if b:
+            continue
+        else:answer.append(todo_list[i])
     return answer
