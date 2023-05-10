b5ad24f6860b735cc64b6cab7ae6c44399b38d64
kyooon6248 <62235442+kyooon6248@users.noreply.github.com>
2023-05-10 13:04:52 +0900

    [unrated] Title: n개 간격의 원소들, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.py
+++ 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.py
@@ -1,6 +1,2 @@
 def solution(num_list, n):
-    answer = []
-    for i, num in enumerate(num_list):
-        if i % n == 0:
-            answer.append(num)
-    return answer
+    return num_list[::n]
