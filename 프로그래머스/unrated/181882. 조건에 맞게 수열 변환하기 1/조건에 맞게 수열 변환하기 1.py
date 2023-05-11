a94cac6bd0a5869e03a6b3bad7fd3c832e6e20a7
kyooon6248 <62235442+kyooon6248@users.noreply.github.com>
2023-05-11 11:53:07 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 1, Time: 22.50 ms, Memory: 15.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.py
@@ -0,0 +1,7 @@
+def solution(arr):
+    for i, num in enumerate(arr):
+        if num>=50 and num%2==0:
+            arr[i] = num//2
+        elif num<50 and num%2==1:
+            arr[i] = num*2
+    return arr
